30cac3306e83066b1e51983043385864887f6b84
kyu12k <kikejess2@gmail.com>
2026-03-17 18:56:08 +0900

    기억 다지기 멘트 수정

--- game.js
+++ game.js
@@ -2737,7 +2737,7 @@ function openModeSelect(stageId) {
             overlay.style = 'display:flex; position:fixed; z-index:9999; top:0; left:0; width:100vw; height:100vh; background:rgba(44,62,80,0.97); color:#f1c40f; flex-direction:column; align-items:center; justify-content:center; font-size:1.2rem; text-align:center;';
             overlay.innerHTML = `
                 <div style="max-width:90vw; font-size:1.1em; line-height:1.7; font-weight:bold; color:#fff; text-shadow:0 2px 8px #222; margin-bottom:30px; flex-shrink: 0;">
-                    🕑 기억 다지기 보너스 스테이지<br><span style="font-size:0.95em; color:#f1c40f;">스테이지를 복습해서 기억을 다지세요!(보상 5배)</span>
+                    🕑 기억 다지기 보너스 스테이지<br><span style="font-size:0.95em; color:#f1c40f;">스테이지를 복습해서 <br>기억을 다지세요!(보상 5배)</span>
                 </div>
                 <div id="forgotten-stages-list" style="background:rgba(0,0,0,0.3); padding:20px; border-radius:15px; width:90vw; max-width:600px; min-width:220px; margin-bottom:30px; max-height:50vh; overflow-y:auto; flex-shrink: 0;">
                 </div>
